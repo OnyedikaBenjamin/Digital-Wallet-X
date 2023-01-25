68291004b4a777369f65becfce634e7b2a5a28e4
Racheal <holuwanifemijoke@gmail.com>
2023-01-25 09:01:11 +0100

    login_impl

--- src/main/java/com/africa/semicolon/ewallet/services/user/UserService.java
+++ src/main/java/com/africa/semicolon/ewallet/services/user/UserService.java
@@ -1,10 +1,13 @@
 package com.africa.semicolon.ewallet.services.user;
 
 import com.africa.semicolon.ewallet.data.models.User;
+import com.africa.semicolon.ewallet.dtos.request.LoginRequest;
 
 import java.util.Optional;
 
 public interface UserService {
     String createAccount(User user);
+
+    String login(LoginRequest loginRequest);
     Optional<User>findUserByEmailAddress(String EmailAddress);
 }
--- src/main/java/com/africa/semicolon/ewallet/services/user/UserServiceImpl.java
+++ src/main/java/com/africa/semicolon/ewallet/services/user/UserServiceImpl.java
@@ -3,12 +3,15 @@ package com.africa.semicolon.ewallet.services.user;
 import com.africa.semicolon.ewallet.data.models.ConfirmationOTP;
 import com.africa.semicolon.ewallet.data.models.User;
 import com.africa.semicolon.ewallet.data.repositories.UserRepo;
+import com.africa.semicolon.ewallet.dtos.request.LoginRequest;
+import com.africa.semicolon.ewallet.exceptions.GenericHandlerException;
 import com.africa.semicolon.ewallet.services.registration.otp.ConfirmationOTPService;
 import com.africa.semicolon.ewallet.utils.OTPGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -29,6 +32,15 @@ public class UserServiceImpl implements UserService{
         );
         confirmationOTPService.saveConfirmationOTP(confirmationOTP);
         return oTP;
+    }
+
+    @Override
+    public String login(LoginRequest loginRequest) {
+        User foundUser = userRepo.findUserByEmailAddressIgnoreCase(loginRequest.getEmailAddress())
+                .orElseThrow(()-> new GenericHandlerException("User with " + loginRequest.getEmailAddress() + "does not exist "));
+        if (foundUser.getIsDisabled())throw new GenericHandlerException("Verify account");
+        if (!Objects.equals(foundUser.getPassword(), loginRequest.getPassWord()))throw new GenericHandlerException("Login incorrect");
+        return "Login successful";
     }
 
     @Override
