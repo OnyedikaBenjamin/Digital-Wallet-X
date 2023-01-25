3ff73b6eb24c3d24ee8f08a476a39ab6a2e2efdc
Anu A <anuoluwaakinsola1@gmail.com>
2023-01-25 16:04:22 +0100

    changePassword completed

--- src/main/java/com/africa/semicolon/ewallet/controllers/UserController.java
+++ src/main/java/com/africa/semicolon/ewallet/controllers/UserController.java
@@ -1,5 +1,6 @@
 package com.africa.semicolon.ewallet.controllers;
 
+import com.africa.semicolon.ewallet.dtos.request.ChangePasswordRequest;
 import com.africa.semicolon.ewallet.dtos.request.LoginRequest;
 import com.africa.semicolon.ewallet.services.user.UserService;
 import com.africa.semicolon.ewallet.utils.ApiResponse;
@@ -7,10 +8,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.ZonedDateTime;
 
@@ -35,5 +33,18 @@ public class UserController {
         return new ResponseEntity<>(apiResponse, HttpStatus.ACCEPTED);
 
 
+    }
+
+    @PutMapping("/change-password")
+    public ResponseEntity<?>changePassword(@RequestBody ChangePasswordRequest changePasswordRequest, HttpServletRequest httpServletRequest) {
+        String changePasswordResponse = userService.changePassword(changePasswordRequest);
+        ApiResponse apiResponse =       ApiResponse.builder()
+                .timeStamp(ZonedDateTime.now())
+                .data(changePasswordResponse)
+                .path(httpServletRequest.getRequestURI())
+                .statusCode(HttpStatus.OK.value())
+                .isSuccessful(true)
+                .build();
+        return new ResponseEntity<>(apiResponse, HttpStatus.ACCEPTED);
     }
 }
--- src/main/java/com/africa/semicolon/ewallet/services/user/UserService.java
+++ src/main/java/com/africa/semicolon/ewallet/services/user/UserService.java
@@ -2,6 +2,7 @@ package com.africa.semicolon.ewallet.services.user;
 
 import com.africa.semicolon.ewallet.data.models.User;
 
+import com.africa.semicolon.ewallet.dtos.request.ChangePasswordRequest;
 import com.africa.semicolon.ewallet.dtos.request.LoginRequest;
 
 import jakarta.transaction.Transactional;
@@ -16,6 +17,7 @@ public interface UserService {
     String login(LoginRequest loginRequest);
     Optional<User>findUserByEmailAddress(String EmailAddress);
 
+    String changePassword(ChangePasswordRequest changePasswordRequest);
     void enableUser(String emailAddress);
     void saveUser(User user);
 }
