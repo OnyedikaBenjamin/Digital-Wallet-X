26846cfe50b1ba52d534a1454cc83e961f63f5b1
Anu A <anuoluwaakinsola1@gmail.com>
2023-01-26 16:57:04 +0100

    next of kin service

--- src/main/java/com/africa/semicolon/ewallet/data/repositories/NextOfKinRepo.java
+++ src/main/java/com/africa/semicolon/ewallet/data/repositories/NextOfKinRepo.java
@@ -1,8 +1,10 @@
 package com.africa.semicolon.ewallet.data.repositories;
 
+import com.africa.semicolon.ewallet.data.models.NextOfKin;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 @Repository
-public interface NextOfKin extends JpaRepository<NextOfKin, Long> {
+public interface NextOfKinRepo extends JpaRepository<NextOfKin, Long> {
+
 }
