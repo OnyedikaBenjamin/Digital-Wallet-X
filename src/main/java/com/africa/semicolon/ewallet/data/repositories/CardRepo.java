af719a1b55ecc61a548456dbd0bc7c7eb450b9b1
Ben Billion <107368386+Udegbunamonyedikabenjamin@users.noreply.github.com>
2023-01-26 16:11:31 +0100

    CardRepo Updated

--- src/main/java/com/africa/semicolon/ewallet/data/repositories/CardRepo.java
+++ src/main/java/com/africa/semicolon/ewallet/data/repositories/CardRepo.java
@@ -1,7 +1,9 @@
 package com.africa.semicolon.ewallet.data.repositories;
 
+import com.africa.semicolon.ewallet.data.models.Card;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 @Repository
-public interface CardRepo{
+public interface CardRepo extends JpaRepository<Card, Long>{
 }
