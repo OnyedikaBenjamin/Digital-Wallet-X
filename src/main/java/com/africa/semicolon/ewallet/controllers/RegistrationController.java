3c5efffe0b2c3af71cde3aab2307a7f1e1ac1c3c
Ben Billion <107368386+Udegbunamonyedikabenjamin@users.noreply.github.com>
2024-02-04 08:44:58 +0100

    Resolve merge conflicts

--- src/main/java/com/africa/semicolon/ewallet/controllers/RegistrationController.java
+++ src/main/java/com/africa/semicolon/ewallet/controllers/RegistrationController.java
@@ -1,36 +1,27 @@
 package com.africa.semicolon.ewallet.controllers;
 
 import com.africa.semicolon.ewallet.dtos.request.RegistrationRequest;
-<<<<<<< HEAD
 import com.africa.semicolon.ewallet.services.registration.otp.RegistrationService;
 import com.africa.semicolon.ewallet.utils.ApiResponse;
-=======
 import com.africa.semicolon.ewallet.dtos.request.SendOTPRequest;
 import com.africa.semicolon.ewallet.dtos.request.VerifyOTPRequest;
 import com.africa.semicolon.ewallet.services.registration.otp.RegistrationService;
 import com.africa.semicolon.ewallet.utils.ApiResponse;
 import jakarta.mail.MessagingException;
->>>>>>> aee9af0e7b39e430cb08826f8cf583fa2f823c76
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-<<<<<<< HEAD
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-=======
 import org.springframework.web.bind.annotation.*;
->>>>>>> aee9af0e7b39e430cb08826f8cf583fa2f823c76
 
 import java.time.ZonedDateTime;
 
 @RestController
-<<<<<<< HEAD
-=======
 @CrossOrigin(origins = "http://localhost:3000")
->>>>>>> aee9af0e7b39e430cb08826f8cf583fa2f823c76
 @RequestMapping("/api/v1/registration")
 public class RegistrationController {
     @Autowired
@@ -41,7 +32,6 @@ public class RegistrationController {
                                       HttpServletRequest httpServletRequest){
 =======
                                       HttpServletRequest httpServletRequest) throws MessagingException {
->>>>>>> aee9af0e7b39e430cb08826f8cf583fa2f823c76
         String createUser = registrationService.register(registrationRequest);
         ApiResponse apiResponse = ApiResponse.builder()
                 .timeStamp(ZonedDateTime.now())
