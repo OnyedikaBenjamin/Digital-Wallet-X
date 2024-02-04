545ffefd65c6e723d4b7e940a25d1465c2535dbd
Ben Billion <107368386+Udegbunamonyedikabenjamin@users.noreply.github.com>
2024-02-04 08:49:41 +0100

    Resolve merge conflicts

--- src/main/java/com/africa/semicolon/ewallet/controllers/RegistrationController.java
+++ src/main/java/com/africa/semicolon/ewallet/controllers/RegistrationController.java
@@ -12,7 +12,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-<<<<<<< HEAD
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
