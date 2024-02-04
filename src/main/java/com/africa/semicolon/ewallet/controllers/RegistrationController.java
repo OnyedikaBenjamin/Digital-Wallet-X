75ba57a8fbe0cd260307710c06a1f8c720b874b0
Ben Billion <107368386+Udegbunamonyedikabenjamin@users.noreply.github.com>
2024-02-04 08:48:42 +0100

    Resolve merge conflicts

--- src/main/java/com/africa/semicolon/ewallet/controllers/RegistrationController.java
+++ src/main/java/com/africa/semicolon/ewallet/controllers/RegistrationController.java
@@ -3,33 +3,44 @@ package com.africa.semicolon.ewallet.controllers;
 import com.africa.semicolon.ewallet.dtos.request.RegistrationRequest;
 import com.africa.semicolon.ewallet.services.registration.otp.RegistrationService;
 import com.africa.semicolon.ewallet.utils.ApiResponse;
+=======
 import com.africa.semicolon.ewallet.dtos.request.SendOTPRequest;
 import com.africa.semicolon.ewallet.dtos.request.VerifyOTPRequest;
 import com.africa.semicolon.ewallet.services.registration.otp.RegistrationService;
 import com.africa.semicolon.ewallet.utils.ApiResponse;
 import jakarta.mail.MessagingException;
+>>>>>>> aee9af0e7b39e430cb08826f8cf583fa2f823c76
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+<<<<<<< HEAD
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+=======
 import org.springframework.web.bind.annotation.*;
+>>>>>>> aee9af0e7b39e430cb08826f8cf583fa2f823c76
 
 import java.time.ZonedDateTime;
 
 @RestController
+<<<<<<< HEAD
+=======
 @CrossOrigin(origins = "http://localhost:3000")
+>>>>>>> aee9af0e7b39e430cb08826f8cf583fa2f823c76
 @RequestMapping("/api/v1/registration")
 public class RegistrationController {
     @Autowired
     private RegistrationService registrationService;
     @PostMapping("/register")
     public ResponseEntity<?> register(@RequestBody RegistrationRequest registrationRequest,
+<<<<<<< HEAD
                                       HttpServletRequest httpServletRequest){
+=======
                                       HttpServletRequest httpServletRequest) throws MessagingException {
+>>>>>>> aee9af0e7b39e430cb08826f8cf583fa2f823c76
         String createUser = registrationService.register(registrationRequest);
         ApiResponse apiResponse = ApiResponse.builder()
                 .timeStamp(ZonedDateTime.now())
@@ -41,6 +52,9 @@ public class RegistrationController {
 
         return new ResponseEntity<>(apiResponse, HttpStatus.CREATED);
     }
+<<<<<<< HEAD
+=======
+
 
     @PostMapping("/verify")
     public ResponseEntity<?>verifyOTP(@RequestBody VerifyOTPRequest verifyOTPRequest,
@@ -72,4 +86,6 @@ public class RegistrationController {
 
         return new ResponseEntity<>(apiResponse, HttpStatus.OK);
     }
+
+>>>>>>> aee9af0e7b39e430cb08826f8cf583fa2f823c76
 }
